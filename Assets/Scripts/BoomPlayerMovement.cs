5bac6d3272a10a96ebc8f49e4705f5fca7c36579
linkft <tgvita@outlook.com>
2023-09-18 16:15:12 +0800

    some bool states and data fields

--- Assets/Scripts/BoomPlayerMovement.cs
+++ Assets/Scripts/BoomPlayerMovement.cs
@@ -7,12 +7,29 @@ using static TFR.InputAsset;
 public class BoomPlayerMovement : MonoBehaviour
 {
     #region Input
+    [Header("Input Router")]
     [SerializeField]
     private InputRouter inputRouter;
     private MovementActions movement;
     #endregion
 
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
+    [Space(5)]
+    [SerializeField] private LayerMask groundLayer;
+
+    #region State Params
+    public bool IsFacingRight { get; private set; }
+    public bool IsJumping { get; private set; }
+    public bool IsInExplosionCooldown { get; private set; }
+    #endregion
+
+    #region Check Params
+    //Set all of these up in the inspector
+    [Header("Checks")]
+    [SerializeField] private Transform _groundCheckPoint;
+    //Size of groundCheck depends on the size of your character generally you want them slightly small than width (for ground) and height (for the wall check)
+    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
+    #endregion
     void Awake()
     {
         movement = inputRouter.Movement;
@@ -22,6 +39,9 @@ public class BoomPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.
+        var move = movement.Move.ReadValue<float>();
+        var jumpStart = movement.Jump.WasPressedThisFrame();
+        var jumpEnd = movement.Jump.WasReleasedThisFrame();
+        var explodePressed = movement.Explode.IsPressed();
     }
 }
--- Assets/Scripts/Reference/PlayerData.cs
+++ Assets/Scripts/Reference/PlayerData.cs
@@ -3,7 +3,7 @@
 
 namespace Dawnosaur
 {
-    [CreateAssetMenu(menuName = "Player Data")] //Create a new playerData object by right clicking in the Project Menu then Create/Player/Player Data and drag onto the player
+    //[CreateAssetMenu(menuName = "Player Data")] //Create a new playerData object by right clicking in the Project Menu then Create/Player/Player Data and drag onto the player
     public class PlayerData : ScriptableObject
 	{
 		[Header("Gravity")]
